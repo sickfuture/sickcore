4949337451a99853d47773be2eb2afb38d8ad0ce
alex dzeshko <a.dzeshko@gmail.com>
2013-08-13 23:38:25 +0300

    string utils

---
+++ Sickcore/src/com/android/sickfuture/sickcore/utils/StringsUtils.java
@@ -0,0 +1,36 @@
+package com.android.sickfuture.sickcore.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.content.ContentValues;
+
+public class StringsUtils {
+
+	public static <T> String join(List<T> items, String joiner) {
+		String ids = "";
+		for (int i = 0; i < items.size(); i++) {
+			ids += String.valueOf(items.get(i)) + joiner;
+		}
+		return ids.substring(0, ids.length() - 1);
+	}
+	
+	public static String join(HashMap<?, ?> items, String joiner) {
+		String keys = "";
+		String[] k = new String[items.size()];
+		items.keySet().toArray(k);
+		for (int i = 0; i < k.length; i++) {
+			keys += k[i] + joiner;
+		}
+		return keys.substring(0, keys.length() - 1);
+	}
+	
+	public static String join(ArrayList<ContentValues> items, String itemKey, String joiner) {
+		String keys = "";
+		for (int i = 0; i < items.size(); i++) {
+			keys += items.get(i).getAsString(itemKey) + joiner;
+		}
+		return keys.substring(0, keys.length() - 1);
+	}
+}
